4adb7977b79e0d72257af6789bebda3f21456915
kav2019 <kav2019@mail.ru>
2021-02-01 17:28:41 +0300

    hw 7

---
+++ Lesson7/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        TestingClass.start(TestingClass.class);
+    }
+}
---
+++ Lesson7/src/main/java/MethodsPriority.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Method;
+
+public class MethodsPriority {
+    public Method method;
+    public Integer priority;
+
+    public MethodsPriority(Method method, int priority) {
+        this.method = method;
+        this.priority = priority;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+}
